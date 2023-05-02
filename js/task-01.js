2f29b696ab53e6fba3cd3dd0244ddfa90d3c6130
RosVor <ros.vorokh@gmail.com>
2023-05-03 02:27:46 +0300

    add js task 1

---
+++ js/task-01.js
@@ -0,0 +1,14 @@
+const categories = document.querySelector("#categories")
+const liItem = document.querySelector(".item")
+
+// console.log('Number of categories: ${liItem.lenght}');
+var items = document.ul.li.length;
+document.write('Number of categories: '+ items);
+
+liItem.forEach((item => {
+    const headings = item.querySelector("h2");
+    const list = item.querySelector("ul");
+    const item = item.querySelectorAll("li");
+}));
+console.log('Category: ${headings.textContent}');
+console.log('Elements: ${item.lenght}');
